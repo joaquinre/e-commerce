efbf6c8946455b9686d16fa7cb7d7a5608eb760b
Joaquin Re <joaquinre123@gmail.com>
2021-07-25 19:37:21 -0300

    added custom error Handler

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRouter.js'
@@ -17,6 +18,9 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes)
 
+app.use(notFound)
+app.use(errorHandler)
+
 app.get('/api/products', (req, res) => {
 	res.json(products)
 })
@@ -25,8 +29,6 @@ app.get('/api/products/:id', (req, res) => {
 	const product = products.find(p => p._id === req.params.id)
 	res.json(product)
 })
-
-
 const PORT = process.env.PORT || 5000
 
 
